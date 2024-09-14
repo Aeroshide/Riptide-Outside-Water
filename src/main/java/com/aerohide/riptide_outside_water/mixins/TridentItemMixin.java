5acbf95ffc1e426b6a62d391e1af634ec570eeeb
Aeroshide <huskyisbadboi61@gmail.com>
2024-09-14 17:12:22 +0700

    1.21 version. might do multiversion but im lazy right now

--- src/main/java/com/aerohide/riptide_outside_water/mixins/TridentItemMixin.java
+++ src/main/java/com/aerohide/riptide_outside_water/mixins/TridentItemMixin.java
@@ -27,6 +27,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static com.aerohide.riptide_outside_water.Riptide_outside_water.cooldownTime;
+
 
 @Mixin(TridentItem.class)
 public class TridentItemMixin<T> extends Item {
@@ -36,10 +38,10 @@ public class TridentItemMixin<T> extends Item {
 
     @Unique
     boolean useOutsideWater = false;
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true, require = 0)
     private void useTrident(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
         useOutsideWater = false;
-        if (!user.isTouchingWaterOrRain() && EnchantmentHelper.getRiptide(user.getStackInHand(hand)) > 0)
+        if (!user.isTouchingWaterOrRain() && EnchantmentHelper.getTridentSpinAttackStrength(user.getStackInHand(hand), user) > 0.0F)
         {
             useOutsideWater = true;
             user.setCurrentHand(hand);
@@ -50,68 +52,12 @@ public class TridentItemMixin<T> extends Item {
     }
 
 
-    @Inject(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
-    public void useOutsideWater(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci) {
-        if (user instanceof PlayerEntity playerEntity) {
-            int HeldTime = this.getMaxUseTime(stack) - remainingUseTicks;
-            if (HeldTime >= 10) {
-                int RiptideLevel = EnchantmentHelper.getRiptide(stack);
-                if (RiptideLevel <= 0 || useOutsideWater) {
-                    if (!world.isClient) {
-                        stack.damage(1, playerEntity, (p) -> {
-                            p.sendToolBreakStatus(user.getActiveHand());
-                        });
-                        if (RiptideLevel == 0) {
-                            TridentEntity tridentEntity = new TridentEntity(world, playerEntity, stack);
-                            tridentEntity.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, 2.5F + (float)RiptideLevel * 0.5F, 1.0F);
-                            if (playerEntity.getAbilities().creativeMode) {
-                                tridentEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
-                            }
-
-                            world.spawnEntity(tridentEntity);
-                            world.playSoundFromEntity((PlayerEntity)null, tridentEntity, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                            if (!playerEntity.getAbilities().creativeMode) {
-                                playerEntity.getInventory().removeOne(stack);
-                            }
-                        }
-                    }
-
-                    playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
-                    if (RiptideLevel > 0) {
-                        ((PlayerEntity) user).getItemCooldownManager().set(stack.getItem(), Riptide_outside_water.cooldownTime);
-                        float f = playerEntity.getYaw();
-                        float g = playerEntity.getPitch();
-                        float h = -MathHelper.sin(f * 0.017453292F) * MathHelper.cos(g * 0.017453292F);
-                        float k = -MathHelper.sin(g * 0.017453292F);
-                        float l = MathHelper.cos(f * 0.017453292F) * MathHelper.cos(g * 0.017453292F);
-                        float m = MathHelper.sqrt(h * h + k * k + l * l);
-                        float n = 3.0F * ((1.0F + (float)RiptideLevel) / 4.0F);
-                        h *= n / m;
-                        k *= n / m;
-                        l *= n / m;
-                        playerEntity.addVelocity((double)h, (double)k, (double)l);
-                        playerEntity.useRiptide(20);
-                        if (playerEntity.isOnGround()) {
-                            float o = 1.1999999F;
-                            playerEntity.move(MovementType.SELF, new Vec3d(0.0, 1.1999999284744263, 0.0));
-                        }
-
-                        SoundEvent soundEvent;
-                        if (RiptideLevel >= 3) {
-                            soundEvent = SoundEvents.ITEM_TRIDENT_RIPTIDE_3;
-                        } else if (RiptideLevel == 2) {
-                            soundEvent = SoundEvents.ITEM_TRIDENT_RIPTIDE_2;
-                        } else {
-                            soundEvent = SoundEvents.ITEM_TRIDENT_RIPTIDE_1;
-                        }
-
-                        world.playSoundFromEntity((PlayerEntity)null, playerEntity, soundEvent, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                    }
-
-                }
-            }
-        }
-
+    @Redirect(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
+    public boolean empty2(PlayerEntity instance) {
+        if (useOutsideWater)
+            instance.getItemCooldownManager().set(instance.getStackInHand(instance.getActiveHand()).getItem(), cooldownTime);
+        return true;
     }
+
 
 }
