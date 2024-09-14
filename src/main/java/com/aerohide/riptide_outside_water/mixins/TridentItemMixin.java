a02e4e6d6f24fa48cb2c1b04bafa1ef42c89b579
Aeroshide <huskyisbadboi61@gmail.com>
2024-09-14 15:28:46 +0700

    mabuk gw

---
+++ src/main/java/com/aerohide/riptide_outside_water/mixins/TridentItemMixin.java
@@ -0,0 +1,116 @@
+package com.aerohide.riptide_outside_water.mixins;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.TridentItem;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+@Mixin(TridentItem.class)
+public class TridentItemMixin<T> extends Item {
+    public TridentItemMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Unique
+    boolean useOutsideWater = false;
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    private void useTrident(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+        useOutsideWater = false;
+        if (!user.isTouchingWaterOrRain() && EnchantmentHelper.getRiptide(user.getStackInHand(hand)) > 0)
+        {
+            useOutsideWater = true;
+            user.setCurrentHand(hand);
+            cir.setReturnValue(TypedActionResult.consume(user.getStackInHand(hand)));
+            cir.cancel();
+        }
+
+    }
+
+
+    @Inject(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
+    public void useOutsideWater(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci) {
+        if (user instanceof PlayerEntity playerEntity) {
+            int HeldTime = this.getMaxUseTime(stack) - remainingUseTicks;
+            if (HeldTime >= 10) {
+                int RiptideLevel = EnchantmentHelper.getRiptide(stack);
+                if (RiptideLevel <= 0 || useOutsideWater) {
+                    if (!world.isClient) {
+                        stack.damage(1, playerEntity, (p) -> {
+                            p.sendToolBreakStatus(user.getActiveHand());
+                        });
+                        if (RiptideLevel == 0) {
+                            TridentEntity tridentEntity = new TridentEntity(world, playerEntity, stack);
+                            tridentEntity.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, 2.5F + (float)RiptideLevel * 0.5F, 1.0F);
+                            if (playerEntity.getAbilities().creativeMode) {
+                                tridentEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+                            }
+
+                            world.spawnEntity(tridentEntity);
+                            world.playSoundFromEntity((PlayerEntity)null, tridentEntity, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                            if (!playerEntity.getAbilities().creativeMode) {
+                                playerEntity.getInventory().removeOne(stack);
+                            }
+                        }
+                    }
+
+                    playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+                    if (RiptideLevel > 0) {
+                        ((PlayerEntity) user).getItemCooldownManager().set(stack.getItem(), 200);
+                        float f = playerEntity.getYaw();
+                        float g = playerEntity.getPitch();
+                        float h = -MathHelper.sin(f * 0.017453292F) * MathHelper.cos(g * 0.017453292F);
+                        float k = -MathHelper.sin(g * 0.017453292F);
+                        float l = MathHelper.cos(f * 0.017453292F) * MathHelper.cos(g * 0.017453292F);
+                        float m = MathHelper.sqrt(h * h + k * k + l * l);
+                        float n = 3.0F * ((1.0F + (float)RiptideLevel) / 4.0F);
+                        h *= n / m;
+                        k *= n / m;
+                        l *= n / m;
+                        playerEntity.addVelocity((double)h, (double)k, (double)l);
+                        playerEntity.useRiptide(20);
+                        if (playerEntity.isOnGround()) {
+                            float o = 1.1999999F;
+                            playerEntity.move(MovementType.SELF, new Vec3d(0.0, 1.1999999284744263, 0.0));
+                        }
+
+                        SoundEvent soundEvent;
+                        if (RiptideLevel >= 3) {
+                            soundEvent = SoundEvents.ITEM_TRIDENT_RIPTIDE_3;
+                        } else if (RiptideLevel == 2) {
+                            soundEvent = SoundEvents.ITEM_TRIDENT_RIPTIDE_2;
+                        } else {
+                            soundEvent = SoundEvents.ITEM_TRIDENT_RIPTIDE_1;
+                        }
+
+                        world.playSoundFromEntity((PlayerEntity)null, playerEntity, soundEvent, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                    }
+
+                }
+            }
+        }
+
+    }
+
+}
