16edfe40670ed2523cd4b2c0fdace86b29cd5ed5
Aeroshide <huskyisbadboi61@gmail.com>
2025-05-13 07:29:26 +0700

    Fixed Client-Server desync on wether can riptide or not
    Fixed Riptide spin behavior by returning SUCCESS on client and CONSUME on server in `TridentItem.use` to preserve animation and boost, while allowing right-clickable offhand items.

--- src/main/java/com/aeroshide/riptide_outside_water/mixins/TridentItemMixin.java
+++ src/main/java/com/aeroshide/riptide_outside_water/mixins/TridentItemMixin.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.entity.event.v1.EntityElytraEvents;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -25,6 +26,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Objects;
 
+import static com.aeroshide.riptide_outside_water.Riptide_outside_water.LOG;
 import static com.aeroshide.riptide_outside_water.Riptide_outside_water.cooldownTime;
 
 
@@ -38,24 +40,32 @@ public class TridentItemMixin<T> extends Item {
     boolean useOutsideWater = false;
     @Inject(method = "use", at = @At("HEAD"), cancellable = true, require = 0)
     private void useTrident(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-        useOutsideWater = false;
+        boolean canSpin = !user.isTouchingWaterOrRain()
+                && EnchantmentHelper.getTridentSpinAttackStrength(user.getStackInHand(hand), user) > 0.0F
+                && (!world.isClient || (RiptideOutsideWaterClient.clientAllowMod  // or your DataTracker flag
+                || MinecraftClient.getInstance().isIntegratedServerRunning()));
 
-
-        if ((!user.isTouchingWaterOrRain() && EnchantmentHelper.getTridentSpinAttackStrength(user.getStackInHand(hand), user) > 0.0F) && (!world.isClient || RiptideOutsideWaterClient.clientAllowMod))
-        {
+        if (canSpin) {
             useOutsideWater = true;
             user.setCurrentHand(hand);
-            cir.setReturnValue(ActionResult.CONSUME);
+
+            if (world.isClient) {
+                cir.setReturnValue(ActionResult.SUCCESS);
+            } else {
+                cir.setReturnValue(ActionResult.CONSUME);
+            }
             cir.cancel();
         }
-
     }
+
 
 
     @Redirect(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
     public boolean empty2(PlayerEntity instance) {
-        if (useOutsideWater)
+        if (useOutsideWater && !instance.getWorld().isClient) {
             instance.getItemCooldownManager().set(instance.getStackInHand(instance.getActiveHand()), cooldownTime);
+            useOutsideWater = false;
+        }
         return true;
     }
 
