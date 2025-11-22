925807ebea5cfabeed7b2ca461c04095118ba10e
Aeroshide <huskyisbadboi61@gmail.com>
2025-11-23 03:25:36 +0700

    fun fact: im very dumb

--- src/main/java/com/aeroshide/riptide_outside_water/mixins/MinecraftClientMixin.java
+++ src/main/java/com/aeroshide/riptide_outside_water/mixins/MinecraftClientMixin.java
@@ -1,12 +1,15 @@
 package com.aeroshide.riptide_outside_water.mixins;
 
 import com.aeroshide.riptide_outside_water.RiptideOutsideWaterClient;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Environment(EnvType.CLIENT)
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 
--- src/main/java/com/aeroshide/riptide_outside_water/mixins/TridentItemMixin.java
+++ src/main/java/com/aeroshide/riptide_outside_water/mixins/TridentItemMixin.java
@@ -3,10 +3,12 @@ package com.aeroshide.riptide_outside_water.mixins;
 import com.aeroshide.riptide_outside_water.AllowModPayload;
 import com.aeroshide.riptide_outside_water.RiptideOutsideWaterClient;
 import com.aeroshide.riptide_outside_water.Riptide_outside_water;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.entity.event.v1.EntityElytraEvents;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
@@ -42,8 +44,7 @@ public class TridentItemMixin<T> extends Item {
     private void useTrident(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         boolean canSpin = !user.isTouchingWaterOrRain()
                 && EnchantmentHelper.getTridentSpinAttackStrength(user.getStackInHand(hand), user) > 0.0F
-                && (!world.isClient() || (RiptideOutsideWaterClient.clientAllowMod  // or your DataTracker flag
-                || MinecraftClient.getInstance().isIntegratedServerRunning()));
+                && (!world.isClient() || RiptideOutsideWaterClient.clientAllowMod || isIntegratedServer());
 
         if (canSpin) {
             useOutsideWater = true;
@@ -59,6 +60,22 @@ public class TridentItemMixin<T> extends Item {
     }
 
 
+    @Unique
+    // equally bad code, in a rush.
+    private static boolean isIntegratedServer() {
+        try {
+            if (FabricLoader.getInstance().getEnvironmentType() != EnvType.CLIENT) return false;
+
+            Class<?> mcClass = Class.forName("net.minecraft.client.MinecraftClient");
+            java.lang.reflect.Method getInstance = mcClass.getMethod("getInstance");
+            Object mc = getInstance.invoke(null);
+            java.lang.reflect.Method isIntegrated = mcClass.getMethod("isIntegratedServerRunning");
+            Object val = isIntegrated.invoke(mc);
+            return val instanceof Boolean && (Boolean) val;
+        } catch (Throwable t) {
+            return false;
+        }
+    }
 
     @Redirect(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
     public boolean empty2(PlayerEntity instance) {
